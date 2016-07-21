4ec65e9bfedb856ec30bfd15d94ad7bed894566f
Colin Young <me@colinyoung.com>
2016-07-21 14:20:01 -0500

    Elastic Beanstalk wants 200 OK

--- lib/wet/health_endpoint/middleware.rb
+++ lib/wet/health_endpoint/middleware.rb
@@ -11,7 +11,7 @@ module Wet
 
       def _call(env)
         status, headers, body = @app.call(env)
-        return [204, {}, ['']] if status == 404 && env.fetch('PATH_INFO') == '/health'
+        return [200, {}, ['']] if status == 404 && env.fetch('PATH_INFO') == '/health'
         [status, headers, body]
       ensure
         body.close if body && body.respond_to?(:close) && $!
